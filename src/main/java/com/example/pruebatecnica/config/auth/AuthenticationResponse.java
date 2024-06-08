1c48b3c032bc8adb09937fbb7b58dc8d56ef2331
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-06-08 14:00:33 -0500

    auth done

---
+++ src/main/java/com/example/pruebatecnica/config/auth/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.example.pruebatecnica.config.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+}
---
+++ src/main/java/com/example/pruebatecnica/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.pruebatecnica.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
